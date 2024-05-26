15f238ae5e291513766cf72c4ae6f307a3a1e7d8
Caetano <Caetanojob05@gmail.com>
2024-05-26 18:14:12 -0300

    API Evento e Pesquisa

---
+++ src/main/java/com/example/senac_marketing/repository/PesquisaRepository.java
@@ -0,0 +1,9 @@
+package com.example.senac_marketing.repository;
+
+import com.example.senac_marketing.modal.Pesquisa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PesquisaRepository extends JpaRepository<Pesquisa, Long> {
+}
---
+++ src/main/java/com/example/senac_marketing/resourse/PesquisaController.java
@@ -0,0 +1,47 @@
+package com.example.senac_marketing.resourse;
+
+import com.example.senac_marketing.modal.Pesquisa;
+import com.example.senac_marketing.service.PesquisaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/Pesquisa")
+public class PesquisaController {
+
+    @Autowired
+    private PesquisaService ServicePesquisa;
+
+
+    @PostMapping
+    public ResponseEntity create(@RequestBody Pesquisa entity){
+        Pesquisa save = ServicePesquisa.salvar(entity);
+        return ResponseEntity.created(URI.create("/api/Pesquisa/" + entity.getId())).body(save);
+    }
+
+    @GetMapping
+    public  ResponseEntity findAll(){
+        List<Pesquisa> pesquisas = ServicePesquisa.buscaTodos();
+        return ResponseEntity.ok(pesquisas);
+    }
+    @GetMapping("/{id}")
+    public  ResponseEntity findById(@PathVariable("id") Long id){
+        Pesquisa pesquisas = ServicePesquisa.buscaPorId(id);
+        return ResponseEntity.ok().body(pesquisas);
+    }
+    @DeleteMapping("/{id}")
+    public  ResponseEntity remove(@PathVariable("id") Long id){
+        ServicePesquisa.remover(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("{id}")
+    public  ResponseEntity update(@PathVariable("id") Long id, @RequestBody Pesquisa entity){
+        Pesquisa alterado = ServicePesquisa.alterar(id, entity);
+        return  ResponseEntity.ok().body(alterado);
+    }
+}
