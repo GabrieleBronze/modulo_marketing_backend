e7f4d336dd0a106bf65d37bd97ea6f801b2abe77
André M <andreminuzzi10@gmail.com>
2024-07-08 21:19:45 -0300

    Inicio do desenvolvimento da DTO de IndicadorDTO

--- src/main/java/com/example/senac_marketing/resourse/representation/IndicadorDTO.java
+++ src/main/java/com/example/senac_marketing/resourse/representation/IndicadorDTO.java
@@ -1,4 +1,38 @@
 package com.example.senac_marketing.resourse.representation;
 
+import com.example.senac_marketing.modal.Indicador;
+
 public class IndicadorDTO {
+
+    private  String nome;
+    private String descricao;
+    private  String setor;
+
+    public String getNome() {return nome; }
+
+    public void setNome(String nome) {this.nome = nome; }
+
+    public String getDescricao() {return descricao; }
+
+    public void setDescricao(String descricao) {this.descricao = descricao; }
+
+    public String getSetor() {return setor; }
+
+    public void setSetor(String setor) {this.setor = setor; }
+
+    public static IndicadorDTO fromEntity(Indicador indicador){
+        IndicadorDTO dto = new IndicadorDTO();
+        dto.setNome(indicador.getNome());
+        dto.setDescricao(indicador.getDescricao());
+        dto.setSetor(indicador.getSetor());
+
+        return  dto;
+    }
+
+    public  Indicador toEntity()  {
+        Indicador indicador =  new Indicador();
+        indicador.setNome(this.getNome());
+        indicador.setDescricao((this.getDescricao()));
+
+    }
 }
