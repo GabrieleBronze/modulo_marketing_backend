0d211355a585c7b54899a33ad7824f0205ac6999
Samsung <gabibronze17@gmail.com>
2024-07-09 20:05:53 -0300

    Adicionando o método PesquisaDTO.

--- src/main/java/com/example/senac_marketing/resourse/representation/PesquisaDTO.java
+++ src/main/java/com/example/senac_marketing/resourse/representation/PesquisaDTO.java
@@ -1,4 +1,86 @@
 package com.example.senac_marketing.resourse.representation;
 
+import com.example.senac_marketing.modal.Evento;
+import com.example.senac_marketing.modal.Pesquisa;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class PesquisaDTO {
+
+    private String nome;
+
+    private LocalDate periodoInicio;
+
+    private LocalDate periodoFim;
+
+    private String setor;
+
+    private String descricao;
+
+
+    // Getters e Setters
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getPeriodoInicio() {
+        return periodoInicio;
+    }
+
+    public void setPeriodoInicio(LocalDate periodoInicio) {
+        this.periodoInicio = periodoInicio;
+    }
+
+    public LocalDate getPeriodoFim() {
+        return periodoFim;
+    }
+
+    public void setPeriodoFim(LocalDate periodoFim) {
+        this.periodoFim = periodoFim;
+    }
+
+    public String getSetor() {
+        return setor;
+    }
+
+    public void setSetor(String setor) {
+        this.setor = setor;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    // Métodos de conversão
+
+    public static PesquisaDTO fromEntity(Pesquisa pesquisa) {
+        PesquisaDTO dto = new PesquisaDTO();
+        dto.setNome(pesquisa.getNome());
+        dto.setPeriodoInicio(pesquisa.getPeriodoInicio());
+        dto.setPeriodoFim(pesquisa.getPeriodoFim());
+        dto.setSetor(pesquisa.getSetor());
+        dto.setDescricao(pesquisa.getDescricao());
+        return dto;
+    }
+    public static List<PesquisaDTO> fromEntity(List<Pesquisa> Pesquisa) {
+        return Pesquisa.stream().map(PesquisaDTO::fromEntity).collect(Collectors.toList());
+    }
+    public static Page<PesquisaDTO> fromEntity(Page<Pesquisa> Pesquisa) {
+        List<PesquisaDTO> clientesFind = Pesquisa.stream().map(PesquisaDTO::fromEntity).collect(Collectors.toList());
+        return new PageImpl<>(clientesFind, Pesquisa.getPageable(), Pesquisa.getTotalElements());
+    }
+
 }
